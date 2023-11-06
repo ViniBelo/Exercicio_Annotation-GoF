6fd43b12dbdb43fd1005262324962815dd7549e9
ViniBelo <vinialvesbelo@outlook.com>
2023-11-06 08:14:52 -0300

    Test on dynamic proxy

--- src/test/java/proxies/DynamicProxyInterceptorTest.java
+++ src/test/java/proxies/DynamicProxyInterceptorTest.java
@@ -8,24 +8,31 @@ import org.junit.jupiter.api.Test;
 import utils.UserPermissions;
 import utils.UserRoles;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ProxyTest {
+class DynamicProxyInterceptorTest {
 
     static UserDao dao;
 
-    Proxy proxy;
+    static InvocationHandler handler;
+
+    proxies.Proxy proxy;
 
     @BeforeAll
     static void beforeAll() {
         dao = new UserDaoStub();
+        UserDao dao = new UserDaoImpl();
+        handler = new DynamicProxyInterceptor(dao);
     }
 
     @Test
-    void save() {
+    void invoke() {
         // given
         User user = new User("Teste", UserRoles.ADMINISTRATOR, UserPermissions.MODERATION);
-        proxy = new Proxy(dao);
+        proxy = new proxies.Proxy((UserDao) Proxy.newProxyInstance(dao.getClass().getClassLoader(), new Class[]{UserDao.class}, handler));
 
         // then
         User savedUser = proxy.save(user);
